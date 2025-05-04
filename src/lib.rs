6669dab0716781440060ffa776d4477b6704b1e4
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-05-04 14:17:05 -0700

    Made it so you don't have to load the audio every time you want to play it
    The function to load and play the audio at a certain path is still there
    but now its called play_audio_path() and theres also a load_audio()
    function

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::io::BufReader;
 #[allow(unused_imports)]
 use rodio::{Decoder, OutputStream};
 
-pub fn play_audio(
+pub fn play_audio_path(
     path: &str,
     volume: f32,
     sink: &rodio::Sink
@@ -18,6 +18,26 @@ pub fn play_audio(
     sink.append(menu_loop_sound);
     sink.play();
     sink.set_volume(volume);
+}
+
+pub fn play_audio(
+    audio: Decoder<BufReader<File>>,
+    volume: f32,
+    sink: &rodio::Sink
+) {
+    sink.append(audio);
+    sink.play();
+    sink.set_volume(volume);
+}
+
+pub fn load_audio(
+    path: &str
+) -> Decoder<BufReader<File>> {
+    return Decoder::new(
+        BufReader::new(
+            File::open(path).unwrap()
+        )
+    ).unwrap()
 }
 
 pub fn stop_audio(
@@ -43,7 +63,9 @@ mod tests {
         let (_stream, stream_handle) = OutputStream::try_default().unwrap();
         let sink = rodio::Sink::try_new(&stream_handle).unwrap();
 
-        play_audio("./menu-music.mp3", 2.0, &sink);
+        let test_sound = load_audio("./menu-music.mp3");
+
+        play_audio_path("./menu-music.mp3", 2.0, &sink);
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
@@ -51,7 +73,7 @@ mod tests {
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
-        play_audio("./menu-music.mp3", 2.0, &sink);
+        play_audio(test_sound, 2.0, &sink);
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
